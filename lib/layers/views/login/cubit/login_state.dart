49d86e67fbce5450d3d68b13846421d829ff3755
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2024-03-04 12:38:59 -0500

    Dependencies missing added

--- lib/layers/views/login/cubit/login_state.dart
+++ lib/layers/views/login/cubit/login_state.dart
@@ -1,6 +1,10 @@
 part of 'login_cubit.dart';
 
-@immutable
-sealed class LoginState {}
+sealed class LoginState extends Equatable {
+  const LoginState();
+
+  @override
+  List<Object> get props => [];
+}
 
 final class LoginInitial extends LoginState {}
