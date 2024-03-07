5b6c690d27dc02ab9bc687ede1b8886f48244d91
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2024-03-07 11:45:45 -0500

    .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:resq/firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+  await dotenv.load(fileName: '.env');
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
