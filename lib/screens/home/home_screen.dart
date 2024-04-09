de0394514414b4e3a081a4f41cb6766bd1444c2e
NicoleMurillo <n.murillof@uniandes.edu.co>
2024-04-08 20:17:34 -0500

    Código inicial - No se ha probado en emulador

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,24 +3,131 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:resq/blocs/sign_in_bloc/sign_in_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-			appBar: AppBar(
-				title: const Text(
-					'Welcome, you are In !'
-				),
-				actions: [
-					IconButton(
-						onPressed: () {
-							context.read<SignInBloc>().add(const SignOutRequired());
-						}, 
-						icon: const Icon(Icons.login)
-					)
-				],
-			),
-		);
+      appBar: AppBar(
+        title: const Text('Homepage'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.read<SignInBloc>().add(const SignOutRequired());
+            },
+            icon: const Icon(Icons.login),
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Container(
+              width: 200,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text(
+                  'Homepage',
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          // Espacio para el newsletter
+          // Botones
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción del primer botón
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromRGBO(80, 225, 130, 1),
+                    ),
+                    shape: MaterialStateProperty.all<OutlinedBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  child: Text('Contact the student brigade'),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción del segundo botón
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromRGBO(80, 225, 130, 1),
+                    ),
+                    shape: MaterialStateProperty.all<OutlinedBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  child: Text('Report MAAD case'),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción del tercer botón
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromRGBO(80, 225, 130, 1),
+                    ),
+                    shape: MaterialStateProperty.all<OutlinedBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  child: Text('Safety tips on campus'),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          // Botón deslizador
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Text(
+                  'Activate alerts',
+                  style: TextStyle(color: Colors.black, fontSize: 12),
+                ),
+                Spacer(),
+                Switch(
+                  value: true, // Cambiar por el valor de activación
+                  onChanged: (value) {
+                    // Acá se pone la acción cuando cambia el switch 
+                  },
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          // Espacio para el mapa con geolocalización
+          Expanded(
+            child: Placeholder(), // Placeholder para el mapa
+          ),
+        ],
+      ),
+    );
   }
 }
+
