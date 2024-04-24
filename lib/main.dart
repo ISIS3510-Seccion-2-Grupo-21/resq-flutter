7b308314dc5d3b92d7cb132c14b9abe3c7aa56a6
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2024-04-24 10:12:35 -0500

    Eventual Connectivity

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:resq/screens/auth/login_process.dart';
-import 'package:resq/screens/home/home_screen.dart';// Importa tu pantalla de inicio
 import 'package:resq/firebase_options.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:resq/screens/auth/login_process.dart';
 import 'package:user_repository/user_repository.dart';
-import 'package:resq/screens/chat/chat_view.dart';
-import 'package:permission_handler/permission_handler.dart'; // Importa el paquete de manejo de permisos
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,19 +12,58 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
-  // Set up Firebase Crashlytics
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
-
-  // Request permissions
-  await requestPermissions();
 
   // Run the app
-  runApp(LoginProcess(FirebaseUserRepo())); // Cambia LoginProcess por MyApp
+  runApp(MyApp());
 }
 
-Future<void> requestPermissions() async {
-  // Request camera and storage permissions
-  await Permission.camera.request();
-  await Permission.storage.request();
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InternetCheckScreen(),
+    );
+  }
+}
+
+class InternetCheckScreen extends StatefulWidget {
+  @override
+  _InternetCheckScreenState createState() => _InternetCheckScreenState();
+}
+
+class _InternetCheckScreenState extends State<InternetCheckScreen> {
+  bool _isConnected = true;
+
+  @override
+  void initState() {
+    super.initState();
+    // Check for internet connection immediately
+    checkInternetConnection();
+    // Listen for changes in connectivity status
+    Connectivity().onConnectivityChanged.listen((result) {
+      setState(() {
+        _isConnected = result[0] != ConnectivityResult.none;
+      });
+    });
+  }
+
+  Future<void> checkInternetConnection() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    setState(() {
+      _isConnected = connectivityResult[0] != ConnectivityResult.none;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_isConnected) {
+      return const Scaffold(
+        body: Center(
+          child: Text('No internet connection!'),
+        ),
+      );
+    } else {
+      return LoginProcess(FirebaseUserRepo());
+    }
+  }
 }
